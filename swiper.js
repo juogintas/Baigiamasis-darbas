b6e2b251ea98cbe60394cbb76c22ce19364ccaff
juogintas <jonas.uog@gmail.com>
2022-12-21 01:09:32 +0200

    6

--- swiper.js
+++ swiper.js
@@ -6,12 +6,8 @@ const swiper = new Swiper('.swiper', {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+
   
   });
